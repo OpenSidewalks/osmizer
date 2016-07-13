2ac344dea5dabc2e96a49a28c856e2d86576a6e2
Kaicheng Tan <tan95@cs.washington.edu>
2016-07-13 16:39:12 -0700

    Change Note: + Validate GeoJSON with custom schema + test file + sample schema

--- GeoJSONtoOSM.py
+++ GeoJSONtoOSM.py
@@ -1,18 +1,35 @@
 import geopandas as gpd
+import fiona
 import click
+import json
+import jsonschema
+import sys
 
 
-def read_input(file_path):
+def validate_input(json_file, schema_file):
     try:
-        file_read = gpd.read_file(file_path, mode='r', driver='GeoJSON')
+        json_in = json.load(json_file)
+        schema_in = json.load(schema_file)
+        jsonschema.validate(json_in, schema_in)
+    except json.decoder.JSONDecodeError:
+        click.echo("Input JSON fail to be decoded")
+        return False
+    except jsonschema.ValidationError:
+        click.echo("Input JSON fail to match schema")
     except:
+        print("Unexpected error:", sys.exc_info()[0])
+        return False
+
+    return True
+
+
+def read_input(json_path):
+    try:
+        file_read = gpd.read_file(json_path, mode='r', driver='GeoJSON')
+    except fiona.errors.FionaValueError:
+        click.echo('Failed to read as GeoJSON')
         return -1
     return file_read
-
-
-def validate_input(gpd_database):
-    # TODO: Validate Input
-    return True
 
 
 def to_OSM(gpd_database, output_path):
@@ -21,30 +38,38 @@ def to_OSM(gpd_database, output_path):
 
 
 @click.command()
-@click.option('--validate/--no-validate', default=True, help='Validate the input GeoJSON file before conversion')
+@click.option('--validate/--no-validate', default=True,
+              help='Validate the input GeoJSON file before conversion')
 @click.argument('file_in', type=click.Path(exists=True, readable=True, allow_dash=True))
 @click.argument('file_out', type=click.Path(exists=False, writable=True, allow_dash=True))
-def converter(file_in, file_out, validate):
+@click.argument('validate_schema', default='Schema/GeoJSONSchema.json',
+                type=click.Path(exists=True, readable=True, allow_dash=True))
+def converter(file_in, file_out, validate, validate_schema):
     click.echo('File in: ' + file_in)
     click.echo('File out: ' + file_out)
+    click.echo('...')
+
+    if validate:
+        if validate_input(open(file_in), open(validate_schema)):
+            click.echo('Checked: Valid GeoJSON Input File')
+            click.echo('...')
+        else:
+            click.echo('ERROR: Non-Valid GeoJSON Input File')
+            click.echo('Operation Terminated')
+            return
 
     database = read_input(file_in)
     if database == -1:
         click.echo('Failed to Read Input File')
+        click.echo('Operation Terminated')
         return
     else:
         click.echo('Input File Read Successfully')
-
-    if validate:
-        if validate_input(database):
-            click.echo('Checked: Valid Input File')
-        else:
-            click.echo('Checked: Non-Valid Input File')
-            click.echo('Operation Terminated')
-            return
+        click.echo('...')
 
     if to_OSM(database, file_out):
         click.echo('OSM file saved')
+        click.echo('...')
     else:
         click.echo('OSM file failed to save')
         click.echo('Operation Terminated')

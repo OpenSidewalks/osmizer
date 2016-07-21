9e18e0bf0cd8f9153881bcc7cc895a600af3f902
Meg Drouhard <meg.drouhard@gmail.com>
2016-07-21 16:22:43 -0700

    Very small tests for cli params

---
+++ tests/test_cli_params.py
@@ -0,0 +1,34 @@
+import click
+from click.testing import CliRunner
+from nose.tools import *
+
+import GeoJSON2OSM as gj2o
+
+runner = CliRunner()
+
+def test_invalid_option():
+    try:
+        @click.command()
+        @click.option('foo')
+        def cli(foo):
+            pass
+    except TypeError as e:
+        assert 'No options defined but a name was passed (foo).' \
+            in str(e)
+    else:
+        assert False, 'Expected a type error because of an invalid option.'
+
+def test_help_text():
+    """Tests appropriate running and output text with help parameter."""
+    result = runner.invoke(gj2o.cli,["--help"])
+    assert result.exit_code == 0
+
+def test_cli_params():
+    test_invalid_option()
+    test_help_text()
+
+
+
+if __name__ == '__main__':
+    runner = CliRunner()
+    test_cli_params()
